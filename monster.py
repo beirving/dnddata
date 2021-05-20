87da1a1a98074aedf4ca06f7d46bdd61b5367549
Bradley Irving <30394346+beirving@users.noreply.github.com>
2021-05-20 12:23:19 -0600

    Update monster.py
    remove verifications

--- monster.py
+++ monster.py
@@ -5,7 +5,7 @@ from lib.monster import Monster
 base_url="http://dnd5eapi.co"
 uri = "/api/monsters/lich"
 
-request = requests.get(base_url+uri)
+request = requests.get(base_url+uri, verify=false)
 data = request.json()
 
 monster = Monster(cred_file_loc="creds/db.json")
@@ -16,6 +16,3 @@ monster.create_or_update("Monster", data, context)
 
 monster.handle_relations(context)
 monster.graph().commit(context)
-
-
-
